5f52906432f4002be1e0fb2ddf4aff842c747593
Iyanu Dayisi <tobiy23@gmail.com>
2024-02-19 10:38:48 +0100

    bubble sorting

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,41 +6,45 @@
  * bubble_sort - sorts an array in ascending order
  * @array: the array to be sorted
  * @size: the size of the array
-*/
+ */
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int isSwapped;
+	size_t i, j;
+	int isSwapped;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        isSwapped = 0;
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                swapValues(&array[j], &array[j + 1]);
-                isSwapped = 1;
-                print_array(array, size);
-            }
-        }
+	if (sizeof(array) == 0)
+		printf(" ");
 
-        if (isSwapped == 0){
-            break;
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		isSwapped = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swapValues(&array[j], &array[j + 1]);
+				isSwapped = 1;
+				print_array(array, size);
+			}
+		}
+
+		if (isSwapped == 0)
+		{
+			break;
+		}
+	}
 }
 
 /**
  * swapValues - swap the values of the array
  * @x: the value to swap
  * @y: the value to be swapped with
-*/
+ */
 
-void swapValues(int* x, int* y)
+void swapValues(int *x, int *y)
 {
-    int temp = *x;
-    *x = *y;
-    *y = temp;
+	int temp = *x;
+	*x = *y;
+	*y = temp;
 }
--- sort.h
+++ sort.h
@@ -11,15 +11,15 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 
 void bubble_sort(int *array, size_t size);
-void swapValues(int* x, int* y);
+void swapValues(int *x, int *y);
 
 #endif
