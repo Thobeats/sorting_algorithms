42a7ce821cd37152cfbd2eac6a1b43e0a2ced565
Iyanu Dayisi <tobiy23@gmail.com>
2024-02-21 01:05:01 +0100

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,58 @@
+#include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * selection_sort - sorts an array with the selection method
+ * @array: the array to be sorted
+ * @size: the size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, j, minId;
+	int isSwapped;
+
+	if (array == NULL)
+		return;
+	if (size <= 0)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		isSwapped = 0;
+		minId = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[minId])
+			{
+				minId = j;
+			}
+		}
+
+		if (minId != i)
+		{
+			swapValues(&array[minId], &array[i]);
+			isSwapped = 1;
+			print_array(array, size);
+		}
+
+		if (isSwapped == 0)
+		{
+			break;
+		}
+	}
+}
+
+/**
+ * swapValues - swap the values of the array
+ * @x: the value to swap
+ * @y: the value to be swapped with
+ */
+
+void swapValues(int *x, int *y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
